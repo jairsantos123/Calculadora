ae0ee19f775db7c6dcc2a8a2d80a487212fe0006
Jairo Rocha <jairabvl@hotmail.com>
2025-05-09 16:54:43 -0300

    ultis.js ok

---
+++ js/utils.js
@@ -0,0 +1,33 @@
+function formatNumber(number) {
+    const numStr = number.toString()
+    if (numStr === "Infinity" || numStr) {
+        return "Erro"
+    }
+
+    if (numStr === "NaN") {
+        return "Erro"
+    }
+
+    const maxDigits = 12
+
+    if (Math.abs(number) >= 1e12) {
+        return number.toExponencial(6)
+    }
+
+    if (Number.isInteger(Number)) {
+        return numStr.toString()
+    }
+
+    const parts = numStr.split(".")
+
+    if (parts[0].lenght >= maxDigits) {
+        return number.toExponencial(6)
+    }
+
+    const decimalPlaces = Math.min (maxDigits - parts[0].lenght - 1, 10)
+    return number.toFixed(decimalPlaces).replace(/\.?0+$/, "")
+}
+
+function isOperador(char) {
+    return ["+", "-", "*", "/"].includes(char)
+}
