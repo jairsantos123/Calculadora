0fc3be1376deaa32d72ce85da5c142b88da79ef7
Jairo Rocha <jairabvl@hotmail.com>
2025-05-09 16:29:16 -0300

    js ok

---
+++ js/theme-switch.js
@@ -0,0 +1,55 @@
+class ThemeSwitcher {
+    constructor() {
+        this.ThemeToggle = document.getElementById("theme-toggle")
+        this.currentTheme = localStorage.getItem("theme") || "light"
+
+        this.initTheme()
+        this.initEventListnres()
+    }
+
+    initTheme() {
+        if (
+            this.currentTheme === "dark" || (!localStorage.getItem("theme") && window.matchMedia("(prefers-color-scheme: dark)"
+        ).matches)
+    ) {
+            document.documentElement.setAttribute("data-theme", "dark")
+            this.currentTheme = "dark"
+        
+        } else {
+            document.documentElement.removeAttribute("data-theme")
+            this.currentTheme = "light"
+        }
+
+        localStorage.setItem("theme", this.currentTheme)
+    }
+
+    initEventListnres() {
+        this.ThemeToggle.addEventListener("click", () => {
+            this.ToggleTheme()
+
+        })
+    window.matchMedia("(prefers-color-scheme: dark)").addEventListener("change", (e) => {
+        if (!localStorage.getItem("theme")) {
+            this.currentTheme = e.matches ? "dark" : "light"
+            this.initTheme()
+        }
+    })
+
+    }
+
+    ToggleTheme() {
+        if (this.currentTheme === "light") {
+            document.documentElement.setAttribute("data-theme", "dark")
+            this.currentTheme = "dark"
+        } else {
+            document.documentElement.removeAttribute("data-theme")
+            this.currentTheme = "light"
+        }
+
+        localStorage.setItem("theme", this.currentTheme)
+    }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    const themeSwitcher = new ThemeSwitcher()
+})
